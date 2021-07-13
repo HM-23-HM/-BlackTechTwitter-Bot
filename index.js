a6f011a85bc30c6237bb2a3fd9164c0967de675e
Thor <73763814+HM-23-HM@users.noreply.github.com>
2021-07-13 02:27:24 -0500

    Attempting to fix over-retweeting error

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Twit = require('twit');
+const Twit = require("twit");
 const T = new Twit({
-    consumer_key: process.env.APPLICATION_CONSUMER_KEY,
-    consumer_secret: process.env.APPLICATION_CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  consumer_key: process.env.APPLICATION_CONSUMER_KEY,
+  consumer_secret: process.env.APPLICATION_CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
-var stream = T.stream('statuses/filter', { track: '#BlackTechTwitter' })
- 
-stream.on('tweet', (tweet) => {
-    T.post('https://api.twitter.com/1.1/statuses/retweet/:id.json', { id: tweet.id_str }, function(err, data, response) {
-            if(err){
-                console.log("Something went wrong. Here is the error ", err)
-            } else {
-                console.log("Success!")
-            }
-          })
-})
+var stream = T.stream("statuses/filter", { track: "#BlackTechTwitter" });
 
-console.log("The bot is running")
+var alreadyRetweeted = [];
+
+stream.on("tweet", (tweet) => {
+  if (!alreadyRetweeted.includes(tweet.id_str)) {
+      if(alreadyRetweeted.length == 5){
+          alreadyRetweeted = []
+      }
+      alreadyRetweeted.push(tweet.id_str)
+    T.post(
+      "https://api.twitter.com/1.1/statuses/retweet/:id.json",
+      { id: tweet.id_str },
+      function (err, data, response) {
+        if (err) {
+          console.log("Something went wrong. Here is the error ", err);
+        } else {
+          console.log("Success!");
+        }
+      }
+    );
+  }
+});
+
+console.log("The bot is running");
