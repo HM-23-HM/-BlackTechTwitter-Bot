621871170c25d6da848bb08906276bf8133b0c8c
Hodeem Miller <miller.hodeem@gmail.com>
2022-12-01 08:39:43 -0500

    It's responding :)

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,14 @@
 require("dotenv").config();
 const app = require("express")();
-// const port = process.env.PORT;
-
 import MainService from "../services/MainService";
 
 app.get("/api", (req, res) => {
+    MainService.run();
+
     res.json({
       message: "You got me"
     })
-    // MainService.run();
 })
-
-// app.listen(port, () => {
-//   console.log("Server is ready to go")
-// })
 
 module.exports = app;
 
