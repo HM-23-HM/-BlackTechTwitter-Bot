746b09622add721e83beec58f8feec91eb355854
Hodeem Miller <miller.hodeem@gmail.com>
2022-12-07 09:44:09 -0500

    Reverting to 1 hr frequency

--- netlify/functions/tweet.ts
+++ netlify/functions/tweet.ts
@@ -1,7 +1,7 @@
 import { schedule } from "@netlify/functions";
 import MainService from "../../services/MainService";
 
-const handler = schedule("0,30 * * * *", async () => {
+const handler = schedule("0 * * * *", async () => {
     const result = await MainService.tweetBatch();
     return result;
 })
